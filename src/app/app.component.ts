aefabb06af332b8022609395fe2afc7423b23613
Mihajlo <nmihajlon@gmail.com>
2025-01-31 03:37:15 +0100

    filter of list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,23 +9,38 @@ import { WishItem } from '../shared/models/wishItem';
 })
 export class AppComponent {
   newWishText? : string;
+  selectedFilter : string = '0';
 
   items : WishItem[] = [
     new WishItem('Buy a new car', true),
     new WishItem('Get a job'),
     new WishItem('Learn to code'),
   ];
+  visibleItems : WishItem[] = this.items;
   
   toggleCheckbox(item : WishItem) : void {
     item.isComplete = !item.isComplete;
-    console.log(item);
+    console.log(this.selectedFilter);
+    this.filterChange(this.selectedFilter);
   }
 
   submitForm(event : Event) : void{
-    event.preventDefault();
     //dodaj wish u listu
     //ocisti text-box
     this.items.push(new WishItem(this.newWishText ?? '', false))
     this.newWishText = '';
+    this.filterChange(this.selectedFilter);
+  }
+
+  filterChange(event : string) : void{
+    if(event === '0'){
+      this.visibleItems = this.items;
+    }
+    else if(event === '1'){
+      this.visibleItems = this.items.filter((item) => item.isComplete === false);
+    }
+    else if(event ==='2'){
+      this.visibleItems = this.items.filter((item) => item.isComplete === true);
+    }
   }
 }
